4806264bed5e8613dc3851d829d79e701f7ae562
salamone-gius <salamone.gius@gmail.com>
2022-04-30 18:10:21 +0200

    commented solution

---
+++ js-hamburger/main.js
@@ -0,0 +1,5 @@
+                // --- TASK --- \\
+// Mostrare / nascondere il menu principale.
+                // - SOLUTION - \\
+// 1. Quando clicco sull'icona dell'hamburger menù, viene mostrato il menù nascosto;
+// 2. quando clicco sull'icona X nel menù nascosto (ora visibile), lo stesso viene di nuovo occultato.
